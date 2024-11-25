d976161a68313978deef88df1ba9e8d8fbf750f3
TsytovychNataliya-FSO <NTsytovych@student.fullsail.edu>
2024-11-24 18:34:46 -0600

    completed the twig forms'

--- sky/index.js
+++ sky/index.js
@@ -1,27 +1,119 @@
-// Load in our Express framework
-const express = require(`express`);
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
-// Create a new Express instance called "app"
 const app = express();
 
+// Use method-override to support DELETE requests
+app.use(methodOverride("_method"));
+
+// Body parser for form data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Only this is necessary for form data
+
 app.set("view engine", "twig");
 app.set("views", __dirname + "/src/views");
 
-// Load in our RESTful routers
-const routers = require("./routers/index.js");
+// In-memory storage for documents
+let documents = [];
 
-// Home page welcome middleware
+// Home route
 app.get("/", (req, res) => {
-  res.status(200).render("home/home", {
-    name: "Justin",
-  });
+  res.status(200).render("home/home");
 });
 
-// Register our RESTful routers with our "app"
-app.use(`/planets`, routers.planet);
-app.use(`/stars`, routers.star);
-app.use(`/galaxies`, routers.galaxy);
+// Route to render the form
+app.get("/add_document", (req, res) => {
+  res.render("partials/add_document");
+});
 
-// Set our app to listen on port 3000
-app.listen(3000);
+// Route to list all documents
+app.get("/document_list", (req, res) => {
+  res.render("partials/document_list", { documents });
+});
+
+// Route to handle form submission (POST)
+app.post("/documents", (req, res) => {
+  const { name, type, description, size } = req.body;
+
+  if (!name || !description || !type || !size) {
+    return res.status(400).send("All fields are required.");
+  }
+
+  let connectedEntities = {};
+  if (type === "planet") {
+    connectedEntities = { star: req.body["planet-star"] };
+  } else if (type === "galaxy") {
+    connectedEntities = {
+      planets: req.body["galaxy-planet"],
+      stars: req.body["galaxy-star"],
+    };
+  } else if (type === "star") {
+    connectedEntities = {
+      planet: req.body["star-planet"],
+      galaxy: req.body["star-galaxy"],
+    };
+  }
+
+  const newDoc = {
+    id: documents.length + 1,
+    name,
+    description,
+    type,
+    size,
+    connected: connectedEntities,
+  };
+
+  documents.push(newDoc);
+  res.redirect("/document_list");
+});
+
+// Route to render the edit form
+app.get("/documents/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const document = documents.find((doc) => doc.id === parseInt(id));
+
+  if (document) {
+    res.render("partials/edit_document", { document });
+  } else {
+    res.status(404).send("Document not found");
+  }
+});
+
+// Route to update a document
+app.put("/documents/:id", (req, res) => {
+  const { id } = req.params;
+  const { description, type, size, connected } = req.body;
+
+  const document = documents.find((doc) => doc.id === parseInt(id));
+
+  if (document) {
+    document.description = description;
+    document.type = type;
+    document.size = size;
+    document.connected = connected;
+
+    res.redirect("/document_list");
+  } else {
+    res.status(404).send("Document not found");
+  }
+});
+
+// Route to delete a document
+app.delete("/documents/:id", (req, res) => {
+  const { id } = req.params;
+
+  // Remove the document by ID
+  documents = documents.filter((doc) => doc.id !== parseInt(id));
+
+  res.redirect("/document_list");
+});
+
+// Register other routers
+const routers = require("./routers/index.js");
+app.use("/planets", routers.planet);
+app.use("/stars", routers.star);
+app.use("/galaxies", routers.galaxy);
+
+// Start the server
+app.listen(3000, () => console.log("Server running on port 3000"));
